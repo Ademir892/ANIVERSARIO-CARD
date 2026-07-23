fda5d56d1447ce22e37e00e42f0579775c3f5f99
Ademir Marmitt Traesel <ademirtraesel@hotmail.com>
2026-07-23 11:33:14 -0300

    feat: generate dynamic starfield background

--- js/background.js
+++ js/background.js
@@ -1,26 +1,58 @@
 const Background = {
 
-    init(){
-
-        console.log("Background iniciado");
+    init() {
 
         this.createNebula();
-
         this.createStars();
-
         this.createMeteors();
 
     },
 
-    createNebula(){
+    createNebula() {
+
+        // Implementaremos depois
 
     },
 
-    createStars(){
+    createStars() {
+
+        this.generateLayer("stars-small", 350, 1, 2);
+
+        this.generateLayer("stars-medium", 120, 2, 3);
+
+        this.generateLayer("stars-big", 30, 3, 5);
 
     },
 
-    createMeteors(){
+    createMeteors() {
+
+        // Implementaremos depois
+
+    },
+
+    generateLayer(id, quantity, min, max) {
+
+        const layer = document.getElementById(id);
+
+        for (let i = 0; i < quantity; i++) {
+
+            const star = document.createElement("span");
+
+            star.classList.add("star");
+
+            const size = Math.random() * (max - min) + min;
+
+            star.style.width = `${size}px`;
+            star.style.height = `${size}px`;
+
+            star.style.left = `${Math.random() * 100}%`;
+            star.style.top = `${Math.random() * 100}%`;
+
+            star.style.opacity = (Math.random() * 0.6 + 0.4).toFixed(2);
+
+            layer.appendChild(star);
+
+        }
 
     }
 
